bbd0dd57a699180aa2e789a1c0ca9b850099a0ce
Nino Kereselidze <your_gitlab_email@example.com>
2025-04-17 05:23:03 +0400

    Initial commit

---
+++ src/main/java/leetcode/easy/problem_0021/README.md
@@ -0,0 +1,7 @@
+# Problem 0021: Merge Two Sorted Lists
+
+🔗 [LeetCode Link](https://leetcode.com/problems/merge-two-sorted-lists/)
+
+## Summary
+
+Merge two sorted linked lists into one sorted linked list and return its head.
---
+++ src/main/java/leetcode/easy/problem_0021/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.easy.problem_0021;
+
+/**
+ * Problem 0021: Merge Two Sorted Lists
+ * <a href="https://leetcode.com/problems/merge-two-sorted-lists/">...</a>
+ * Merge two sorted linked lists and return the head of the merged list.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Alio");
+    }
+}
